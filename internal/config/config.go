9c59118bddc70f81bc986d64187732e2fcff96d7
Daniele Oppezzo <daniele.oppezzo@smeup.com>
2026-02-18 11:04:26 +0100

    modifiche per allineamento a endpoint

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,24 @@
 // internal/config/config.go
+type DBConfig struct {
+	DSN string `yaml:"dsn"`
+}
+
 type APIDestConfig struct {
 	BaseURL string `yaml:"base_url"`
 }
 
+type BatchConfig struct {
+	Size int `yaml:"size"`
+}
+
+type SanitizationConfig struct {
+	AttiveRuleset  string `yaml:"attive_ruleset"`
+	PassiveRuleset string `yaml:"passive_ruleset"`
+}
+
 type Config struct {
-	SourceDB DBConfig      `yaml:"source_db"`
-	APIDest  APIDestConfig `yaml:"api_dest"`
-	Batch    BatchConfig   `yaml:"batch"`
+	SourceDB     DBConfig           `yaml:"source_db"`
+	APIDest      APIDestConfig      `yaml:"api_dest"`
+	Batch        BatchConfig        `yaml:"batch"`
+	Sanitization SanitizationConfig `yaml:"sanitization"`
 }
